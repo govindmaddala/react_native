158875374d3d23b9e08b7211058965f76b849c41
govind maddala <govindmvn@gmail.com>
2024-12-29 22:49:59 +0530

    Bottom and Top tab navigators are added

--- App.js
+++ App.js
@@ -4,14 +4,62 @@ import _1_ViewText from './components/_1_ViewText'
 import _2_Button from './components/_2_Button'
 import _3_Styling from './components/_3_Styling'
 import _4_TextInput from './components/_4_TextInput'
+import _5_Forms from './components/_5_Forms'
+import _6_FlatList from './components/_6_FlatList'
+import _7_List_Map_ScrollView from './components/_7_List_Map_ScrollView'
+import _8_List_Grid_flex from './components/_8_List_Grid_flex'
+import _9_Component_Loop_list from './components/_9_Component_Loop_list'
+import _10_SectionList from './components/_10_SectionList'
+import { _11_ClassComponent } from './components/_11_ClassComponent'
+import _12_Grid_flex from './components/_12_Grid_flex'
+import _13_TouchHighlight from './components/_13_TouchHighlight'
+import _14_RadioButton_TouchOpacity from './components/_14_RadioButton_TouchOpacity'
+import _15_Dynamic_radiobutton from './components/_15_Dynamic_radiobutton'
+import _16_ActivityIndicator_Loader from './components/_16_ActivityIndicator_Loader'
+import _17_Modal from './components/_17_Modal'
+import _18_Pressable_Button from './components/_18_Pressable_Button'
+import _19_StatusBar from './components/_19_StatusBar'
+import _20_Platform from './components/_20_Platform'
+import _21_RN_webview from './components/_21_RN_webview'
+import _22_CustomModal from './components/_22_CustomModal'
+import _23_StackNavigation from './components/_23_0_StackNavigation'
+import _23_StackNavigation_styling from './components/_23_1_StackNavigation_styling'
+import _23_StackNavigation_HeaderComp from './components/_23_2_StackNavigation_HeaderComp'
+import _23_3_StackNavigation_DataPass from './components/_23_3_StackNavigation_DataPass'
+import _24_0_BottomTabNavigator from './components/_24_0_BottomTabNavigator'
+import _24_1_TopTabNavigator from './components/_24_1_TopTabNavigator'
 
 const App = () => {
   return (
     <View style= { {flex: 1, marginTop: 100}}>
-        <_1_ViewText />
+        {/* <_1_ViewText />
         <_2_Button />
         <_3_Styling />
-        <_4_TextInput />
+        <_4_TextInput /> */}
+        {/* <_5_Forms /> */}
+        {/* <_6_FlatList /> */}
+        {/* <_7_List_Map_ScrollView /> */}
+        {/* <_8_List_Grid_flex /> */}
+        {/* <_9_Component_Loop_list /> */}
+        {/* <_10_SectionList /> */}
+        {/* <_11_ClassComponent /> */}
+        {/* <_12_Grid_flex /> */}
+        {/* <_13_TouchHighlight /> */}
+        {/* <_14_RadioButton_TouchOpacity /> */}
+        {/* <_15_Dynamic_radiobutton /> */}
+        {/* <_16_ActivityIndicator_Loader /> */}
+        {/* <_17_Modal /> */}
+        {/* <_18_Pressable_Button /> */}
+        {/* <_19_StatusBar /> */}
+        {/* <_20_Platform /> */}
+        {/* <_21_RN_webview /> */}
+        {/* <_22_CustomModal /> */}
+        {/* <_23_StackNavigation /> */}
+        {/* <_23_StackNavigation_styling /> */}
+        {/* <_23_StackNavigation_HeaderComp /> */}
+        {/* <_23_3_StackNavigation_DataPass /> */}
+        {/* <_24_0_BottomTabNavigator /> */}
+        <_24_1_TopTabNavigator />
     </View>
   )
 }
---
+++ components/_24_1_TopTabNavigator.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { StyleSheet, Text, View } from 'react-native';
+import { TouchableHighlight } from 'react-native';
+
+const Tab = createMaterialTopTabNavigator();
+const _24_1_TopTabNavigator = () => {
+  return (
+    <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name='Login' component={Login} />
+            <Tab.Screen name='Home' component={Home} />
+            <Tab.Screen name='Profile' component={Profile} />
+        </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const Login = (props) => (
+    <View style={styles.main}>
+        <Text>Login Page</Text>
+        <TouchableHighlight onPress={() => props.navigation.navigate("Home")}>
+            <Text style={styles.button}>Get Started</Text>
+        </TouchableHighlight>
+    </View>
+)
+
+const Home = (props) => (
+    <View style={styles.main}>
+        <Text>Home Page</Text>
+        <TouchableHighlight onPress={() => props.navigation.navigate("Profile")}>
+            <Text style={styles.button}>Profile Page</Text>
+        </TouchableHighlight>
+    </View>
+)
+
+const Profile = (props) => (
+    <View style={styles.main}>
+        <Text>Profile Page</Text>
+        <TouchableHighlight onPress={() => props.navigation.navigate("Home")}>
+            <Text style={styles.button}>Go To Home</Text>
+        </TouchableHighlight>
+    </View>
+)
+
+
+
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    button: {
+        width: 200,
+        margin: 10,
+        textAlign: "center",
+        padding: 10,
+        borderRadius: 10,
+        color: "#fff",
+        backgroundColor: "#007bff",
+        borderColor: "#007bff"
+    },
+})
+
+export default _24_1_TopTabNavigator
